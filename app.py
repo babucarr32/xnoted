0ccd491b5b00241f547977d3373669a714eea4c1
babucarr32 <baboucarry@gmail.com>
2025-12-20 21:59:55 +0000

    Hide verstical scroll bar and add binding to body

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from textual.app import App
 from src.screens.createTodo import CreateToDoModal
 from src.components.content import ContentWrapper
+from src.components.body import Body
 
 
 class TodoCLIApp(App):
     CSS_PATH = "src/styles/main.tcss"
     BINDINGS = [
         ("ctrl+n", "create_new_todo", "Create new todo"),
+        ("ctrl+d", "scroll_body_down", "Scroll body down"),
+        ("ctrl+u", "scroll_body_up", "Scroll body up"),
     ]
 
     def compose(self):
@@ -14,3 +17,11 @@ class TodoCLIApp(App):
 
     def action_create_new_todo(self):
         self.app.push_screen(CreateToDoModal())
+
+    def action_scroll_body_down(self):
+        body_widget = self.app.query_one(Body)
+        body_widget.scroll_down()
+
+    def action_scroll_body_up(self):
+        body_widget = self.app.query_one(Body)
+        body_widget.scroll_up()
